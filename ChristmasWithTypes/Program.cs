24c3bb62799ff8d4698a881655e79c9ad10bc7d4
Elijah Blair <elijahblair2005@gmail.com>
2023-12-09 16:01:37 -0500

    Fixed all errors

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -8,20 +8,25 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = Christmas.Day.Thursday;
+            var XmasDay = Christmas.Day.Monday;
 
             //TODO set Santa's name to Kris Kringle
             xmas.Santa = null;
 
+            xmas.Santa = "Kris Kingle";
+
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents = new string[3]{"shoes", "clothes", "toys" };
+
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = null;
 
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
+            xmas.TreeHeight = 10;
+
+            Console.WriteLine($"This year christmas falls on {XmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
+            Console.WriteLine($"Here are the presents we would like: {xmas.Presents}");
 
             foreach (var present in xmas.Presents)
             {
